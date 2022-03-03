a1e0e32e506f187f8adcd977e4f98ed91e930266
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-03-03 03:39:56 +0200

    ded

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -51,13 +51,6 @@ async def gen_thumb(thumbnail, title, userid, status, views, duration, channel):
 
     # Cropping circle from thubnail
     image3 = image11.crop((280,0,1000,720))
-    lum_img = Image.new('L', [720,720] , 0)
-    draw = ImageDraw.Draw(lum_img)
-    draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
-    img_arr =np.array(image3)
-    lum_img_arr =np.array(lum_img)
-    final_img_arr = np.dstack((img_arr,lum_img_arr))
-    image3 = Image.fromarray(final_img_arr)
     image3 = image3.resize((600,600))
 
     image2.paste(image3, (50,70), mask = image3)
